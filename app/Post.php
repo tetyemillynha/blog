2add2235990c79cb19f525fc83d8df2e7851de89
Stefhany Oliveira <stefhany.emilly@hotmail.com>
2019-01-14 17:02:26 -0300

    Inclusão do Projeto

---
+++ app/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable = [
+        'user_id', 
+        'category_id',
+        'name',
+        'slug' ,
+        'execerpt',
+        'body',
+        'status'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::tag);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::tag);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+        
+    $title = $faker->sentence(4);
+    
+        return [
+            //
+            'user_id'     => rand(1,30),
+            'category_id' => rand(1,20),
+            'name'        => $title,
+            'slug'        => str_slug($title),
+            'execerpt'      => $faker->text(200),
+            'body'        => $faker->text(500),
+            'status'      => $faker->randomElement(['DRAFT', 'PUBLISHED'])
+            //'file'        => $faker->imageUrl($width = 1024, $height = 480),
+        
+    ];
+});
