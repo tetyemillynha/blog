06415be8e324a2d4260e640b33bbb4b1765b3bf1
Stefhany Emilly o. dos Santos <stefhany.santos@Intellisys.local>
2019-01-15 09:51:29 -0300

    Criação da página de postagens

---
+++ resources/views/web/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Lista de Artigos</h1>
+        </div>
+    </div>
+@endsection
